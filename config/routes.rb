4f163a363aa2828a2becdd1c394054ad251f6f9f
neomort <kyle@kyleadams.com>
2020-08-31 15:35:17 -0600

    pulled buttons

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,11 @@
 Rails.application.routes.draw do
-  get 'bucket_lists/index'
-  get 'bucket_lists/show'
-  get 'bucket_lists/new'
-  get 'bucket_lists/edit'
-  get 'index/show'
-  get 'index/new'
-  get 'index/edit'
+ 
   root 'home#index'
   devise_for :users
 
   resources :home
   resources :todos
-  
+  resources :bucket_lists
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
 
