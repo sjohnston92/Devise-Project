4746972cf5b9bf2e7e7188ef7f0d6b6580cbc957
sjohnston92 <simonjohnston.c@gmail.com>
2020-08-31 15:06:34 -0600

    Update on todo model with login info

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,13 +1,51 @@
 class TodosController < ApplicationController
   def index
+    @todos = current_user.todos
   end
 
   def show
   end
 
   def new
+    @todo = Todo.new
+  end
+
+  def create
+    @todo = current_user.todos.new(todo_params)
+    if @todo.save
+      redirect_to todos_path
+    else
+      render :new 
+    end
   end
 
   def edit
   end
+
+  def update
+    if @todo.update(account_params)
+      redirect_to todos_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @todo.destroy
+    redirect_to accounts_path
+  end
+
+  private
+
+  def todo_params
+    params.require(:todo).permit(:name, :item)
+  end
+
+  def set_todo
+    @todo = current_user.todos.find(params[:id])
+  end
+
+
+
+  
 end
