8c808b3f244767d37f336b3aaa7117ebb7ed7ca6
neomort <kyle@kyleadams.com>
2020-08-31 17:05:54 -0600

    did not get everything working the way I wanted to

--- app/controllers/bucket_lists_controller.rb
+++ app/controllers/bucket_lists_controller.rb
@@ -1,13 +1,45 @@
 class BucketListsController < ApplicationController
+  before_action :set_bucket_list, only: [:show, :edit]
+
   def index
+    @bucket_lists = BucketList.all
   end
 
   def show
   end
 
   def new
+    @bucket_list = BucketList.new
+    render partial: "form"
   end
+
+  def create
+    #inspect params here
+      #pinding.pry
+    @bucket_list = BucketList.new(bucket_list_params)
+    
+    if @bucket_list.save
+     redirect_to bucket_list_path
+    else
+      render :new
+    end
+    end
 
   def edit
   end
+
+def destroy
+  
+end
+
+private
+
+def bucket_list_params
+params.require(:bucket_list).permit(:name, :location)
+end
+
+def set_bucket_list
+  @bucket_list = BucketList.find(params[:id])
+end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require "faker"
+
+2.times do |i|
+  b = Bucket_list.create(name: "#{i}" Faker::Name.name, location: Faker::Games::Pokemon.location)
+
+end
